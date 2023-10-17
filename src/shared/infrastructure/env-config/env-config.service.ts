ea088c6b434b016b8e51df176b3198377658e988
daavii_b <davi.santos@ax4b.com>
2023-10-16 23:35:34 -0300

    :config: Implements env-config interface

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IEnvConfig } from './env-config.inerface';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements IEnvConfig {
+  constructor(private readonly _configService: ConfigService) {}
+
+  getNodeEnv(): string {
+    return String(this._configService.get<string>('NODE_ENV'));
+  }
+  getAppPort(): number {
+    return Number(this._configService.get<number>('PORT'));
+  }
+}
