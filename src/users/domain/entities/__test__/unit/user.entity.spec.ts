5e1b00b5d829c7929a9f20238ff5b7970e1f58da
daavii_b <davi.santos@ax4b.com>
2023-10-17 22:21:53 -0300

    :feat: Adding getters methods in user entity and adding unit tests to getters

--- src/users/domain/entities/__test__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__test__/unit/user.entity.spec.ts
@@ -20,4 +20,25 @@ describe('UserEntity Unit Test', () => {
     expect(sut.props.password).toBe(userProps.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(typeof sut.name).toBe('string');
+    expect(sut.name).toBe(userProps.name);
+  });
+  it('Getter of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(typeof sut.password).toBe('string');
+    expect(sut.password).toBe(userProps.password);
+  });
+  it('Getter of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(typeof sut.email).toBe('string');
+    expect(sut.email).toBe(userProps.email);
+  });
+  it('Getter of createdAt field', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeInstanceOf(Date);
+    expect(sut.createdAt).toBe(userProps.createdAt);
+  });
 });
