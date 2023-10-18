774297f2670465898f4c0cab02f9adf36b7d7226
daavii_b <davi.santos@ax4b.com>
2023-10-17 23:22:24 -0300

    :feat: Create Setters and Update methods for UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -7,7 +7,13 @@ export type UserProps = {
   createdAt?: Date;
 };
 
-export class UserEntity extends BaseEntity<UserProps> {
+export interface IUserEntity {
+  updateName: (value: string) => void;
+  updateEmail: (value: string) => void;
+  updatePassword: (value: string) => void;
+}
+
+export class UserEntity extends BaseEntity<UserProps> implements IUserEntity {
   constructor(
     public readonly props: UserProps,
     id?: string,
@@ -16,16 +22,40 @@ export class UserEntity extends BaseEntity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
+  updateName(value: string): void {
+    this.name = value;
+  }
+
+  updateEmail(value: string): void {
+    this.email = value;
+  }
+
+  updatePassword(value: string): void {
+    this.password = value;
+  }
+
   get name(): string {
     return this.props.name;
+  }
+
+  private set name(value: string) {
+    this.props.name = value;
   }
 
   get email(): string {
     return this.props.email;
   }
 
+  private set email(value: string) {
+    this.props.email = value;
+  }
+
   get password(): string {
     return this.props.password;
+  }
+
+  private set password(value: string) {
+    this.props.password = value;
   }
 
   get createdAt(): Date {
