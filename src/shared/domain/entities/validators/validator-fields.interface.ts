65c329506d33d730e040be54068111cd8dc893df
daavii_b <davi.santos@ax4b.com>
2023-10-18 08:42:39 -0300

    :feat: Adding Validate Interface

---
+++ src/shared/domain/entities/validators/validator-fields.interface.ts
@@ -0,0 +1,10 @@
+export type FieldsError = {
+  [field: string]: string[];
+};
+
+export interface IValidatorFields<PropsValidated> {
+  errors: FieldsError;
+  validatedData: PropsValidated;
+
+  validate: (data: any) => boolean;
+}
