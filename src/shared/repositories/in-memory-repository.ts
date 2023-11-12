6f806f9ea4f5dfd236639cba9218f10e5601fefa
daavii_b <davi.santos@ax4b.com>
2023-11-12 17:44:21 -0300

    :lipstick: feat: Implementing InMemoryRepository

---
+++ src/shared/repositories/in-memory-repository.ts
@@ -0,0 +1,31 @@
+import { BaseEntity } from '@shared/domain/entities/entity';
+import { IRepository } from './repository-contracts';
+
+export abstract class InMemoryRepository<E extends BaseEntity>
+  implements IRepository<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+  async findById(id: string): Promise<E> {
+    const _id = `${id}`;
+    const entity = this.items.find((item) => item.id === _id);
+
+    if (!entity) {
+      throw new Error('Entity not found');
+    }
+
+    return entity;
+  }
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+  update(entity: E): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
