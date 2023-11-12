609fce19f64db8419a2a8f8ba26239f4c9c3ce70
daavii_b <davi.santos@ax4b.com>
2023-11-12 16:06:37 -0300

    :test: Testing Integration UserEntity Update Method

--- src/users/domain/entities/__test__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__test__/integration/user.entity.int-spec.ts
@@ -50,4 +50,102 @@ describe('UserEntity Integration Tests', () => {
       new UserEntity(props);
     });
   });
+
+  describe('Update Methods', () => {
+    describe('Update Name Method', () => {
+      it('should throw an error if update the user name with invalid data', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updateName');
+
+        expect(() => entity.updateName(12 as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updateName(null as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updateName('')).toThrow(EntityValidatorError);
+        expect(() => entity.updateName('a'.repeat(256))).toThrow(
+          EntityValidatorError,
+        );
+        expect(spyFunction).toHaveBeenCalled();
+      });
+
+      it('should update the user name', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updateName');
+
+        entity.updateName('Another Name');
+
+        expect(entity.name).toBe('Another Name');
+        expect(spyFunction).toHaveBeenCalled();
+      });
+    });
+    describe('Update Email Method', () => {
+      it('should throw an error if update the user email with invalid data', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updateEmail');
+
+        expect(() => entity.updateEmail(12 as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updateEmail(null as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updateEmail('')).toThrow(EntityValidatorError);
+        expect(() => entity.updateEmail('a'.repeat(256))).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updateEmail('invalidemail@com')).toThrow(
+          EntityValidatorError,
+        );
+
+        expect(spyFunction).toHaveBeenCalled();
+      });
+
+      it('should update the user email', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updateEmail');
+
+        entity.updateEmail('valid_useemail@email.com');
+
+        expect(entity.email).toBe('valid_useemail@email.com');
+
+        expect(spyFunction).toHaveBeenCalled();
+      });
+    });
+
+    describe('Update Password Method', () => {
+      it('should throw an error if update the user password with invalid data', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updatePassword');
+
+        expect(() => entity.updatePassword(12 as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updatePassword(null as any)).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updatePassword('')).toThrow(EntityValidatorError);
+        expect(() => entity.updatePassword('a'.repeat(256))).toThrow(
+          EntityValidatorError,
+        );
+        expect(() => entity.updatePassword('weakpasswor11')).toThrow(
+          EntityValidatorError,
+        );
+
+        expect(spyFunction).toHaveBeenCalled();
+      });
+
+      it('should update the user password', () => {
+        const entity = new UserEntity(userDataBuilder({}));
+        const spyFunction = jest.spyOn(entity, 'updatePassword');
+
+        entity.updatePassword('str0NGp@assW0Rd');
+
+        expect(entity.password).toBe('str0NGp@assW0Rd');
+
+        expect(spyFunction).toHaveBeenCalled();
+      });
+    });
+  });
 });
