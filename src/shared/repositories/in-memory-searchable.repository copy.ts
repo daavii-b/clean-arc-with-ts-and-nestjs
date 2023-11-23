3a3b9deee669f00155cae3d9f2ae082fc51574f3
daavii_b <davi.santos@ax4b.com>
2023-11-23 17:39:37 -0300

    :construction: Adding Search, Pagination, Filter methods in the InMemory Repository

--- src/shared/repositories/in-memory-searchable.repository copy.ts
+++ src/shared/repositories/in-memory-searchable.repository copy.ts
@@ -1,12 +1,33 @@
 import { BaseEntity } from '@shared/domain/entities/entity';
 import { InMemoryRepository } from './in-memory.repository';
-import { ISearchableRepository } from './searchable-repository-contract';
+import {
+  ISearchableRepository,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contract';
 
 export abstract class InMemorySearchableRepository<E extends BaseEntity>
   extends InMemoryRepository<E>
-  implements ISearchableRepository<E, any, any>
+  implements ISearchableRepository<E, string, SearchParams, SearchResult<E>>
 {
-  search(props: any): Promise<any> {
+  async search(props: SearchParams): Promise<SearchResult<E>> {
     throw new Error('Method not implemented.');
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+
+  protected async applyPagination(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
