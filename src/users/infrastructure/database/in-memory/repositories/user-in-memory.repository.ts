3b64c9d9f65fd20d489a0b5deb79fddfd20aa6d4
daavii_b <davi.santos@ax4b.com>
2023-11-24 10:58:45 -0300

    :recycle: refact: Fixing somethings in the UserMemoryRepository

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -2,12 +2,18 @@ import { UserEntity } from '@domain/entities/user.entity';
 import { IUserRepository } from '@domain/repositories/user.repository';
 import { ConflictError } from '@shared/domain/errors/conflict-error';
 import { NotFoundError } from '@shared/domain/errors/not-found-error';
-import { InMemorySearchableRepository } from '@shared/repositories/in-memory-searchable.repository copy';
+import { InMemorySearchableRepository } from '@shared/repositories/in-memory-searchable.repository';
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
   implements IUserRepository
 {
+  protected applyFilter(
+    items: UserEntity[],
+    filter: string,
+  ): Promise<UserEntity[]> {
+    throw new Error('Method not implemented.');
+  }
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find((item) => item.email === email);
 
