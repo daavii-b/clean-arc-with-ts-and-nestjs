6b77a1e96ce4674f48e24dc6f19755c661183987
daavii_b <davi.santos@ax4b.com>
2023-11-24 10:51:56 -0300

    :test: Adding Unit test for applyFilter method

--- src/shared/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -30,7 +30,45 @@ describe('InMemorySearchableRepository unit test', () => {
     sut = new StubInMemorySearchableRepository();
   });
 
-  describe('applyFilter method', () => {});
+  describe('applyFilter method', () => {
+    it('should no filter items when filter param is null', async () => {
+      const items = [new StubEntity({ name: 'TestEntity', price: 1.2 })];
+      const spyFilterMethod = jest.spyOn(items, 'filter');
+
+      const itemsFiltered = await sut['applyFilter'](items, null);
+      expect(itemsFiltered).toStrictEqual(items);
+      expect(spyFilterMethod).not.toHaveBeenCalled();
+    });
+
+    it('should filter items using filter param', async () => {
+      const items = [
+        new StubEntity({ name: 'TestEntity', price: 1.2 }),
+        new StubEntity({ name: 'testentity', price: 1.2 }),
+        new StubEntity({ name: 'entity', price: 1.5 }),
+      ];
+
+      const spyFilterMethod = jest.spyOn(items, 'filter');
+
+      let itemsFiltered = await sut['applyFilter'](items, 'TEST');
+
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalled();
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1);
+
+      itemsFiltered = await sut['applyFilter'](items, 'test');
+
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalled();
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+
+      itemsFiltered = await sut['applyFilter'](items, 'empty-filter');
+
+      expect(itemsFiltered).toStrictEqual([]);
+      expect(itemsFiltered).toHaveLength(0);
+      expect(spyFilterMethod).toHaveBeenCalled();
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+    });
+  });
   describe('applySort method', () => {});
   describe('applyPagination method', () => {});
   describe('search method', () => {});
