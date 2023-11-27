5cf5e2a01e763742bee3037ad4b2c31da17310c3
daavii_b <davi.santos@ax4b.com>
2023-11-27 15:00:21 -0300

    :refact: Adding password hash in signup

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { BadRequestError } from '@application/errors/bad-request-error';
 import { UserEntity } from '@domain/entities/user.entity';
 import { NUserRepository } from '@domain/repositories/user.repository';
+import { IHashProvider } from '@shared/application/providers/hash-provider';
 
 export namespace SignUpUseCase {
   export interface ISignUpInput {
@@ -20,6 +21,7 @@ export namespace SignUpUseCase {
   export class UseCase {
     constructor(
       private readonly userRepository: NUserRepository.IRepository,
+      private readonly hashProvider: IHashProvider,
     ) {}
 
     async execute(input: ISignUpInput): Promise<ISignUpOutput> {
@@ -31,7 +33,13 @@ export namespace SignUpUseCase {
 
       await this.userRepository.emailExists(email);
 
-      const entity = new UserEntity(input);
+      const hashPassword = await this.hashProvider.generateHash(password);
+
+      const entity = new UserEntity(
+        Object.assign(input, {
+          password: hashPassword,
+        }),
+      );
 
       await this.userRepository.insert(entity);
 
