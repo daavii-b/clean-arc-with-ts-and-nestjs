0ef6fde8030197dd42721574ee4be1b7ba777198
daavii_b <davi.santos@ax4b.com>
2023-11-27 14:44:39 -0300

    :feat: Adding provider to  hash pass

---
+++ src/users/infrastructure/providers/hash-provider/bcrypt-hash.provider.ts
@@ -0,0 +1,11 @@
+import { IHashProvider } from '@shared/application/providers/hash-provider';
+import { compare, hash } from 'bcryptjs';
+
+export class BcryptHashProvider implements IHashProvider {
+  async generateHash(payload: string): Promise<string> {
+    return hash(payload, 6);
+  }
+  async compareHash(payload: string, hash: string): Promise<boolean> {
+    return compare(payload, hash);
+  }
+}
