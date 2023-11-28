dcfbcd3117e0bc8f65e1e6ec6abd76aa0efd399d
daavii_b <davi.santos@ax4b.com>
2023-11-28 12:51:45 -0300

    :test: Adding Unit tests for ListUserUseCases execute Method

--- src/users/application/usecases/__tests__/unit/list-users.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/list-users.usecase.spec.ts
@@ -56,4 +56,51 @@ describe('ListUsersUseCase unit tests', () => {
       perPage: 1,
     });
   });
+  it('should return the users ordered by createdAt', async () => {
+    const createdAt = new Date();
+    const items = [
+      new UserEntity(userDataBuilder({ createdAt })),
+      new UserEntity(
+        userDataBuilder({ createdAt: new Date(createdAt.getTime() + 2) }),
+      ),
+    ];
+    repository.items = items;
+    const output = await sut.execute({});
+
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map((item) => item.toJSON()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    });
+  });
+  it('should return the users using pagination, sort and filter', async () => {
+    const items = [
+      new UserEntity(userDataBuilder({ name: 'A' })),
+      new UserEntity(userDataBuilder({ name: 'aA' })),
+      new UserEntity(userDataBuilder({ name: 'Ab' })),
+      new UserEntity(userDataBuilder({ name: 'b' })),
+      new UserEntity(userDataBuilder({ name: 'B' })),
+      new UserEntity(userDataBuilder({ name: 'c' })),
+    ];
+
+    repository.items = items;
+
+    const output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    });
+
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON(), items[2].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
+      perPage: 2,
+    });
+  });
 });
