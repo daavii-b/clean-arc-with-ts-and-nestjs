c1d71b4c51a4b26a7575c87b080681905700aa03
daavii_b <davi.santos@ax4b.com>
2023-11-28 11:53:51 -0300

    :feat: ListUsers UseCase

--- src/users/application/usecases/list-users.usecase.ts
+++ src/users/application/usecases/list-users.usecase.ts
@@ -1,11 +1,16 @@
+import {
+  IPaginationOutput,
+  PaginationOutputMapper,
+} from '@shared/application/dtos/pagination-output';
 import { ISearchInput } from '@shared/application/dtos/search-input';
 import { IUseCase } from '@shared/application/usecases/use-case';
 import { NUserRepository } from '@users/domain/repositories/user.repository';
+import { UserOutputMapper } from '../dtos/user-output';
 
 export namespace ListUsersUseCase {
   export interface IListUsersInput extends ISearchInput {}
 
-  export interface IListUsersOutput {}
+  export interface IListUsersOutput extends IPaginationOutput {}
 
   export class UseCase implements IUseCase<IListUsersInput, IListUsersOutput> {
     constructor(
@@ -18,7 +23,17 @@ export namespace ListUsersUseCase {
 
       const searchResult = await this.userRepository.search(params);
 
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(
+      searchResult: NUserRepository.SearchResult,
+    ): IListUsersOutput {
+      const items = searchResult.items.map((item) =>
+        UserOutputMapper.toOutput(item),
+      );
+
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
