ca66ef193c9a98ca0298051f6135828d6dd0e4f4
daavii_b <davi.santos@ax4b.com>
2023-11-29 01:46:11 -0300

    :feat: Implementing Prisma Service

--- src/shared/infrastructure/database/prisma/prisma.service.ts
+++ src/shared/infrastructure/database/prisma/prisma.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit' as never, async () => {
+      await app.close();
+    });
+  }
+}
