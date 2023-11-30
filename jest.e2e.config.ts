f2a9e3303a8d6a22d713610ca98749e987d197f4
daavii_b <davi.santos@ax4b.com>
2023-11-30 17:01:16 -0300

    :feat: Config e2e tests

---
+++ jest.e2e.config.ts
@@ -0,0 +1,17 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testTimeout: 30000,
+};
