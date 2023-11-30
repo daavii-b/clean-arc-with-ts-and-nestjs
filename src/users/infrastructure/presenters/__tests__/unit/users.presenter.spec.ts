4ef7c78b8ba167669467281a397efe60987706d2
daavii_b <davi.santos@ax4b.com>
2023-11-30 14:03:38 -0300

    :test: Adding Unit Test for UserPresenter | part 2

--- src/users/infrastructure/presenters/__tests__/unit/users.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/users.presenter.spec.ts
@@ -1,22 +1,38 @@
+import { instanceToPlain } from 'class-transformer';
 import { UserPresenter } from '../../users.presenter';
 
 describe('UserPresente Unit Tests', () => {
+  const createdAt = new Date();
   const props = {
     id: '69638f89-c731-475f-9bfd-cd858693d7d0',
     name: 'Test User',
     email: 'test@example.com',
     password: 'm@Pas234/',
-    createdAt: new Date(),
+    createdAt,
   };
-  describe('constructor method', () => {
-    it('should set props', () => {
-      const sut = new UserPresenter(props);
+  let sut: UserPresenter;
 
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
+  describe('constructor method', () => {
+    it('should be defined', () => {
       expect(sut).toBeDefined();
       expect(sut.id).toEqual(props.id);
       expect(sut.name).toEqual(props.name);
       expect(sut.email).toEqual(props.email);
       expect(sut.createdAt).toBe(props.createdAt);
+    });
+
+    it('should transform the data to presenter', () => {
+      const output = instanceToPlain(sut);
+
+      expect(output).toStrictEqual({
+        id: '69638f89-c731-475f-9bfd-cd858693d7d0',
+        name: 'Test User',
+        email: 'test@example.com',
+        createdAt: createdAt.toISOString(),
+      });
     });
   });
 });
