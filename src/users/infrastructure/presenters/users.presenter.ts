1b7d813d98bba48de0e6c2e118824403b0202368
daavii_b <britodavi122@gmail.com>
2023-12-12 12:41:51 -0300

    :feat: Adding Doc for UserPresenter

--- src/users/infrastructure/presenters/users.presenter.ts
+++ src/users/infrastructure/presenters/users.presenter.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CollectionPresenter } from '@shared/infrastructure/presenters/collection.presenter';
 import { IUserOutputDTO } from '@users/application/dtos/user-output';
 import { ListUsersUseCase } from '@users/application/usecases/list-users.usecase';
 import { Transform } from 'class-transformer';
 
 export class UserPresenter {
+  @ApiProperty({
+    description: 'User Identification',
+  })
   id: string;
+  @ApiProperty({
+    description: 'User name',
+  })
   name: string;
+  @ApiProperty({
+    description: 'User email',
+  })
   email: string;
+  @ApiProperty({
+    description: 'User creation date',
+  })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
 
